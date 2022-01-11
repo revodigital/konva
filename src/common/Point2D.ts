9270003a5256b0784ee11cbe380cd3909113d768
Gabriele <gabri.cabal@gmail.com>
2022-01-11 11:42:08 +0100

    ✨ NEW: Implements useful Point methods and utils

--- src/common/Point2D.ts
+++ src/common/Point2D.ts
@@ -9,17 +9,139 @@
  * Description:
  */
 
-export interface IPoint {
+import { Vector2d } from '../types';
+
+export interface IPoint2D {
   x: number;
   y: number;
 }
 
-export class Point2D implements IPoint {
+/**
+ * Create a new point. If called without arguments, creates an empty point
+ * @param x
+ * @param y
+ */
+export const pointOf = (x?: number, y?: number): Point2D => {
+  return Point2D.fromXY(x || 0, y || 0);
+}
+
+/**
+ * Represents 2D point into the space.
+ */
+export class Point2D implements IPoint2D {
   x: number;
   y: number;
 
   constructor(x?: number, y?: number) {
     this.x = x || 0;
     this.y = y || 0;
+  }
+
+  /**
+   * Calculates distance between 2 points
+   * @param point Second point
+   */
+  distanceFrom(point: Point2D): number {
+    return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y,
+      2));
+  }
+
+  /**
+   * Checks if this point is the topleft corner of a rectangle
+   */
+  isTopLeft(): boolean {
+    return this.x === 0 && this.y === 0;
+  }
+
+  /**
+   * Normalize undefined values
+   */
+  normalize(): void {
+    if (this.x === undefined) this.x = 0;
+    if (this.y === undefined) this.y = 0;
+  }
+
+  incrementX(value: number): void {
+    if (this.x === undefined) this.x = 0;
+    this.x += value;
+  }
+
+  decrementX(value: number): void {
+    if (this.x === undefined) this.x = 0;
+    this.x -= value;
+  }
+
+  incrementY(value: number): void {
+    if (this.y === undefined) this.y = 0;
+    this.y += value;
+  }
+
+  decrementY(value: number): void {
+    if (this.y === undefined) this.y = 0;
+    this.y -= value;
+  }
+
+  /**
+   * Translate this point by a specific vector
+   * @param vector
+   */
+  translate(vector: Vector2d): void {
+    this.x += vector.x;
+    this.y += vector.y;
+  }
+
+  /**
+   * Get a new point translated of a specific vector
+   * @param vector
+   */
+  translated(vector: Vector2d): Point2D {
+    return new Point2D(this.x + vector.x, this.y + vector.y);
+  }
+
+  /**
+   * Calculates geometric center of the segment between 2 points
+   * @param point
+   */
+  centerOf(point: Point2D): Point2D {
+    return new Point2D(
+      (this.x + point.x) / 2,
+      (this.y + point.y) / 2,
+    );
+  }
+
+  /**
+   * Sets the coordinates of this point passing a vector
+   * @param vector
+   */
+  setVector(vector: Vector2d) {
+    this.x = vector.x;
+    this.y = vector.y;
+  }
+
+  /**
+   * Parses this point starting from a vector
+   * @param vector
+   */
+  static fromVector(vector: Vector2d): Point2D {
+    return new Point2D(vector.x, vector.y);
+  }
+
+  /**
+   * Construct a new point starting from x and y
+   * @param x
+   * @param y
+   */
+  static fromXY(x: number, y: number): Point2D {
+    return new Point2D(x, y);
+  }
+
+  /**
+   * Converts this point into a vector
+   */
+  toVector(): Vector2d {
+    return {
+      x: this.x,
+      y: this.y,
+    };
   }
 }
